67900013187b03dbab260de3cb3eabe1a376d02a
Aly <me@s5.pm>
2024-02-19 23:45:43 -0800

    Improve startup logging

--- ffishlogs/core/src/main/scala/com/ffishlogs/Main.scala
+++ ffishlogs/core/src/main/scala/com/ffishlogs/Main.scala
@@ -1,6 +1,7 @@
 package com.ffishlogs
 
 import cats.effect.{ExitCode, IO, IOApp, Resource}
+import cats.syntax.all.*
 import com.ffishlogs.api.V0
 import com.ffishlogs.db.Db
 import org.http4s.HttpRoutes
@@ -9,25 +10,37 @@ import org.http4s.server.{Router, Server}
 import org.typelevel.log4cats.{Logger, LoggerFactory}
 import org.typelevel.log4cats.slf4j.Slf4jFactory
 import porcupine.Database
+import scala.util.control.NoStackTrace
 
 object Main extends IOApp {
+
+  final case class InitializationError(message: String, cause: Throwable) extends Throwable with NoStackTrace
   
   override def run(args: List[String]): IO[ExitCode] = {
     implicit val loggerFactory: LoggerFactory[IO] = Slf4jFactory.create[IO]
     loggerFactory.create.flatMap { implicit logger: Logger[IO] =>
-      Config.config.load[IO].flatMap { config =>
-        prepareDatabase(config).use { db =>
-          httpServer(db, config).useForever
-        }
+      val server = for {
+        c <- config(logger).evalTap(printConfig(logger, _))
+        db <- database(logger, c)
+        serv <- httpServer(db, c)
+      } yield serv
+      // Print and exit for the errors we know about, hard crash for the ones we don't
+      server.useForever.recoverWith {
+        case InitializationError(message, throwable) =>
+          logger.error(throwable)(message).as(ExitCode.Error)
       }
     }
   }
   
-  def logConfig(logger: Logger[IO], c: Config): IO[Unit] =
+  def printConfig(logger: Logger[IO], c: Config): IO[Unit] =
     logger.info("Config:") >> c.logInfo(logger)
   
-  def prepareDatabase(c: Config): Resource[IO, Db] =
-    Database.open[IO](c.sqlite.database.toString).flatMap(Db.ofDatabase)
+  def config(logger: Logger[IO]): Resource[IO, Config] =
+    Config.config.load[IO].toResource.adaptError(InitializationError("Error loading ffishlogs configuration", _))
+  
+  def database(logger: Logger[IO], c: Config): Resource[IO, Db] =
+    Database.open[IO](c.sqlite.database.toString).adaptError(InitializationError("Error opening ffishlogs database", _))
+      .flatMap(Db.ofDatabase).adaptError(InitializationError("Error initializing ffishlogs database", _))
   
   def httpServer(db: Db, c: Config)(implicit loggerFactory: LoggerFactory[IO]): Resource[IO, Server] =
     Resource.suspend {
